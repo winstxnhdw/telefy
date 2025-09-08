3a071d772f677ddcd9fa713a0268f83134e74c6c
winstxnhdw <winstxnhdw@gmail.com>
2025-09-08 17:06:05 +0800

    refactor: instrument with `opentelemetry`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { cors } from '@elysiajs/cors';
 import { openapi } from '@elysiajs/openapi';
+import { opentelemetry } from '@elysiajs/opentelemetry';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { Elysia } from 'elysia';
 import { notification } from '@/notification';
 import type { Bindings } from '@/types/bindings';
@@ -27,7 +30,25 @@ function main(env: Bindings) {
     },
   });
 
-  return new Elysia({ aot: false }).decorate('env', env).use(scalarPlugin).use(cors()).use(notification());
+  const openTelemetry = opentelemetry({
+    spanProcessors: [
+      new BatchSpanProcessor(
+        new OTLPTraceExporter({
+          url: env.OTEL_EXPORTER_OTLP_ENDPOINT,
+          headers: {
+            Authorization: env.OTEL_EXPORTER_OTLP_TOKEN,
+          },
+        }),
+      ),
+    ],
+  });
+
+  return new Elysia({ aot: false })
+    .decorate('env', env)
+    .use(openTelemetry)
+    .use(scalarPlugin)
+    .use(cors())
+    .use(notification());
 }
 
 export default {
--- src/types/bindings.ts
+++ src/types/bindings.ts
@@ -1,5 +1,7 @@
 export type Bindings = {
   TELEGRAM_CHAT_ID: string;
   TELEGRAM_BOT_TOKEN: string;
+  OTEL_EXPORTER_OTLP_ENDPOINT: string;
+  OTEL_EXPORTER_OTLP_TOKEN: string;
   AUTH_TOKEN: string;
 };
