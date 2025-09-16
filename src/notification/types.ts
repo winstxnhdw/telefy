eb7bfe26df9d9fecbc7317badb7e927f83963a48
Winston H. <56998716+winstxnhdw@users.noreply.github.com>
2025-09-16 22:03:06 +0800

    chore: remove `minLength` for `body`

--- src/notification/types.ts
+++ src/notification/types.ts
@@ -2,6 +2,6 @@ import { t } from 'elysia';
 
 export const NotificationSchema = t.Object({
   subject: t.String({ examples: ['Title'], minLength: 1, maxLength: 96 }),
-  body: t.String({ examples: ['This is the body of the notification.'], minLength: 1, maxLength: 4000 }),
+  body: t.String({ examples: ['This is the body of the notification.'], maxLength: 4000 }),
   attachments: t.Optional(t.Files({ minItems: 1 })),
 });
